c2bb3557098261e6b74c85d58575e914b6b69ed0
Hamidullah <hamidullahh754@gmail.com>
2024-11-28 21:02:10 +0430

    some changes on all the functions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-/* eslint-disable no-eval */
 import { useState } from 'react';
 import './app.css';
 
 export default function Calculator() {
-  const [input, setInput] = useState('');
-  const [result, setResult] = useState('');
+  const [expression, setExpression] = useState([]);
+  const [isResult, setIsResult] = useState(false);
 
   function handleClick(value) {
-    setInput(input + value);
+    const lastItem = expression[expression.length - 1];
+    if (['+', '-', '*', '/'].includes(value) && ['+', '-', '*', '/'].includes(lastItem))
+      return;
+
+    if (isResult) {
+      setExpression([value]);
+      setIsResult(false);
+      setExpression([...expression, value]);
+    } else setExpression([...expression, value]);
   }
 
-  function calculate() {
-    setResult(eval(input));
-    setInput('');
-  }
+  function handleResult() {
+    const result = expression
+      .join('')
+      .split(/(\D)/g)
+      .map((value) => (value.match(/\d/g) ? parseInt(value, 0) : value))
+      .reduce((acc, cur, index, array) => {
+        switch (cur) {
+          case '+':
+            return (acc += array[index + 1]);
+          case '-':
+            return (acc -= array[index + 1]);
+          case '*':
+            return (acc *= array[index + 1]);
+          case '/':
+            return (acc /= array[index + 1]);
+          default:
+            return acc;
+        }
+      });
 
-  function clear() {
-    setInput('');
-    setResult('');
+    if (isNaN(result)) {
+      setExpression([...expression, 'Cannot']);
+    } else {
+      setExpression([result]);
+      setIsResult(true);
+    }
   }
 
   return (
@@ -27,13 +52,10 @@ export default function Calculator() {
           <i className='fa-solid fa-calculator'></i>
         </button>
 
-        <h2 id='result'>
-          {input}
-          {result}
-        </h2>
+        <h2 id='result'>{expression}</h2>
       </div>
       <div className='buttons'>
-        <button onClick={clear} id='clear'>
+        <button onClick={() => setExpression([])} id='clear'>
           C
         </button>
         <button onClick={() => handleClick('%')} id='normal-btn'>
@@ -47,13 +69,13 @@ export default function Calculator() {
         </button>
       </div>
       <div className='buttons'>
-        <button onClick={() => handleClick('7')} id='normal-btn'>
+        <button onClick={() => handleClick(7)} id='normal-btn'>
           7
         </button>
-        <button onClick={() => handleClick('8')} id='normal-btn'>
+        <button onClick={() => handleClick(8)} id='normal-btn'>
           8
         </button>
-        <button onClick={() => handleClick('9')} id='normal-btn'>
+        <button onClick={() => handleClick(9)} id='normal-btn'>
           9
         </button>
         <button onClick={() => handleClick('-')} id='normal-btn'>
@@ -61,13 +83,13 @@ export default function Calculator() {
         </button>
       </div>
       <div className='buttons'>
-        <button onClick={() => handleClick('4')} id='normal-btn'>
+        <button onClick={() => handleClick(4)} id='normal-btn'>
           4
         </button>
-        <button onClick={() => handleClick('5')} id='normal-btn'>
+        <button onClick={() => handleClick(5)} id='normal-btn'>
           5
         </button>
-        <button onClick={() => handleClick('6')} id='normal-btn'>
+        <button onClick={() => handleClick(6)} id='normal-btn'>
           6
         </button>
         <button onClick={() => handleClick('+')} id='normal-btn'>
@@ -75,13 +97,13 @@ export default function Calculator() {
         </button>
       </div>
       <div className='buttons'>
-        <button onClick={() => handleClick('1')} id='normal-btn'>
+        <button onClick={() => handleClick(1)} id='normal-btn'>
           1
         </button>
-        <button onClick={() => handleClick('2')} id='normal-btn'>
+        <button onClick={() => handleClick(2)} id='normal-btn'>
           2
         </button>
-        <button onClick={() => handleClick('3')} id='normal-btn'>
+        <button onClick={() => handleClick(3)} id='normal-btn'>
           3
         </button>
         <button onClick={() => handleClick('.')} id='normal-btn'>
@@ -89,13 +111,13 @@ export default function Calculator() {
         </button>
       </div>
       <div className='buttons'>
-        <button onClick={() => handleClick('0')} id='normal-btn'>
+        <button onClick={() => handleClick(0)} id='normal-btn'>
           0
         </button>
-        <button onClick={() => handleClick(+'00')} id='normal-btn'>
+        <button onClick={() => handleClick(0)} id='normal-btn'>
           00
         </button>
-        <button onClick={() => calculate('=')} id='equalTo'>
+        <button onClick={() => handleResult('=')} id='equalTo'>
           =
         </button>
       </div>
